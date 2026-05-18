9eb0620e062d6c2a51839bf9d38ac34e3c72a674
Giacomo Fulvi <giacomofulvi@Mac.lan>
2026-05-18 11:22:53 +0200

    added air banner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,6 +25,7 @@
 			<p class=" text-gray-600 dark:text-gray-400">
 				VIRGO S.r.l. Strada comunale delle Corti, 54 - 31100 TREVISO - TV - P.IVA: 05543270267
 			</p>
+			<img src="/banner-air.jpeg" alt="Banner AIR" class="mt-6 max-w-xs" />
 		</div>
 		<div>
 			<h2 class="mb-6 text-sm font-semibold text-gray-400 uppercase dark:text-white">Brands</h2>
